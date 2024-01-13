4b368eb7fa7dfe36e5bedeac133ce6ce824ab32c
phabelcev <phabelcev@gmail.com>
2024-01-13 19:20:52 +0300

    firstVersion
    This is the first version of this script, in this version you can create a public repository and upload it to github

--- create.sh
+++ create.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 
+mkdir $1
+touch $1/.gitignore
+
+git init $1/
+
+if [[ $(gh auth status | grep Logged) ]];then
+    gh repo create "$1" --public --add-readme 
+else
+    gh auth login
+    gh repo create "$1" --public --add-readme
+    
+fi
+
+hubName=gh repo view $1 | grep -oP 'name:\s\K\w+'
+
+cd $1
+git remote add origin git@github.com:$hubName/$1.git 
+
+echo "Done!"
